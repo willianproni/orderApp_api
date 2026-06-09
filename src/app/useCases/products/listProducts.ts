21ee11d6422dce1303ad19e9748065a6b1e6c480
willianproni <pronifwillian@gmail.com>
2026-06-09 07:30:27 -0300

    Refactor - UseCase listProducts

--- src/app/useCases/products/listProducts.ts
+++ src/app/useCases/products/listProducts.ts
@@ -5,6 +5,10 @@ export async function listProducts(req: Request, res: Response) {
   try {
     const products = await Product.find();
 
+    if (products.length === 0) {
+      return res.status(204).send();
+    }
+
     res.json(products);
   } catch (error) {
     console.log(error);
