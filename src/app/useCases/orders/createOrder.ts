952542d8d37af51b8076864fa3da100cff6eeef0
willianproni <pronifwillian@gmail.com>
2026-06-10 07:48:38 -0300

    Feat - useCase create orders

---
+++ src/app/useCases/orders/createOrder.ts
@@ -0,0 +1,26 @@
+import type { Request, Response } from "express";
+import { Order } from "../../models/Order.js";
+
+export async function createOrder(req: Request, res: Response) {
+  try {
+    const { table, products } = req.body;
+
+    if (!table) {
+      return res.status(400).json({ error: "Table is required" });
+    }
+
+    if (products.length === 0) {
+      return res.status(400).json({ error: "Products are required" });
+    }
+
+    const order = await Order.create({
+      table,
+      products,
+    });
+
+    res.status(201).json({ order });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
